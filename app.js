fb4f1cee37f2405facb3bad8af0b4978d6614a73
Ruksana khatun <iitinaruksana@gmail.com>
2025-09-06 12:24:53 +0530

    video-Call and chat code done

--- app.js
+++ app.js
@@ -13,14 +13,3 @@ mobile_btn.addEventListener("click",() => toggleNavbar());
 
 
 
-function checkDevice() {
-    if (window.innerWidth < 1040) {
-        document.body.style.display = 'none';
-        document.getElementById('mobileMessage').style.display = 'block';
-    } else {
-        document.body.style.display = 'block';
-        document.getElementById('mobileMessage').style.display = 'none';
-    }
-}
-window.onload = checkDevice;
-window.onresize = checkDevice;
